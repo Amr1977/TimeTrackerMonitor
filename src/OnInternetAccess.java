13c608aa426f184312569268a7a74bbcff1b9ff7
Amr Lotfy <amr.lotfy@badrit.com>
2014-05-19 11:55:47 +0200

    190514-1155

---
+++ src/OnInternetAccess.java
@@ -0,0 +1,21 @@
+import java.awt.AWTException;
+import java.io.IOException;
+
+import javazoom.jlgui.basicplayer.BasicPlayerException;
+import common.Sikuliz;
+
+
+public class OnInternetAccess extends common.Sikuliz {
+
+	public OnInternetAccess() throws BasicPlayerException, IOException,
+			AWTException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
